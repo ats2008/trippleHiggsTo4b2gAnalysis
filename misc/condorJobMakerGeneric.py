36aadbb73fde2ec11b90d9041f0d78a1883fa1cb
aravind <aravindsugunan@gmail.com>
2022-12-02 12:25:06 +0530

    Ading the argparse based codor job customizer

--- misc/condorJobMakerGeneric.py
+++ misc/condorJobMakerGeneric.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import os
 import sys
+import argparse
+
 version='v1'
 
 useStr="\
@@ -16,6 +18,7 @@ NEVENTS_PER_JOB = -1
 ZERO_OFFSET=0
 FILES_PER_JOB=1
 maxMeterialize=100
+offsetStep=10000
 
 pwd=os.environ['PWD']
 proxy_path=os.environ['X509_USER_PROXY']
@@ -26,38 +29,75 @@ FileSource ="bmm5FileList.txt"
 destination='/grid_mnt/t3storage3/athachay/bs2mumug/run2studies/CMSSW_10_6_19_patch2/src/BsMMGAnalysis/MergeWithBMMNtuples/RunLumiEventFileMaker/runLumiList/'
 tag=""
 
-if len(sys.argv) > 2:
-    executable=sys.argv[1]
-    FileSource=sys.argv[2]  
-    runScriptTemplate=sys.argv[3]  
-    cfgScriptTemplate=sys.argv[4]  
-else:
-    print(useStr)
-    sys.exit(1)
-argC=5
-if len(sys.argv) > argC:
-    destination=sys.argv[argC]  
-argC+=1
-if len(sys.argv) > argC :
-    NJOBS=int(sys.argv[argC])  
-argC+=1
-if len(sys.argv) > argC :
-    FILES_PER_JOB=int(sys.argv[argC])  
-argC+=1
-if len(sys.argv) > argC :
-    NEVENTS_PER_JOB=int(sys.argv[argC])  
-argC+=1
-if len(sys.argv) > argC :
-    tag=sys.argv[argC]  
-argC+=1
-if len(sys.argv) > argC :
-    maxMeterialize=int(sys.argv[argC])  
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--exe", help="Executable")
+parser.add_argument("--fsrc", help="file source")
+parser.add_argument("--runScript", help="template runscript")
+parser.add_argument("--cfg", help="Configuration template file")
+parser.add_argument("--dest", help="destination",default='./')
+parser.add_argument("--jn", help="Max number of Jobs",default='10')
+parser.add_argument("--fn", help="File per Job",default=1)
+parser.add_argument("--maxEvt", help="Mac Events per Job , -1 for all",default=100)
+parser.add_argument("--tag", help="Job Tag",default="")
+parser.add_argument("--offsetStep", help="offsetStep for event number re-defenition",default="5000")
+parser.add_argument("--maxMeterialize", help="Executable",default=None)
+
+args = parser.parse_args()
+
+executable=args.exe
+FileSource=args.fsrc
+runScriptTemplate=args.runScript
+cfgScriptTemplate=args.cfg
+destination=args.dest
+NJOBS=int(args.jn)
+FILES_PER_JOB=int(args.fn)
+NEVENTS_PER_JOB=int(args.maxEvt)
+tag=args.tag
+maxMeterialize=int(args.maxMeterialize)
+offsetStep=int(args.offsetStep)
+
+#if len(sys.argv) > 2:
+#    executable=sys.argv[1]
+#    FileSource=sys.argv[2]  
+#    runScriptTemplate=sys.argv[3]  
+#    cfgScriptTemplate=sys.argv[4]  
+#else:
+#    print(useStr)
+#    sys.exit(1)
+#argC=5
+#if len(sys.argv) > argC:
+#    destination=sys.argv[argC]  
+#argC+=1
+#if len(sys.argv) > argC :
+#    NJOBS=int(sys.argv[argC])  
+#argC+=1
+#if len(sys.argv) > argC :
+#    FILES_PER_JOB=int(sys.argv[argC])  
+#argC+=1
+#if len(sys.argv) > argC :
+#    NEVENTS_PER_JOB=int(sys.argv[argC])  
+#argC+=1
+#if len(sys.argv) > argC :
+#    tag=sys.argv[argC]  
+#argC+=1
+#if len(sys.argv) > argC :
+#    maxMeterialize=int(sys.argv[argC])  
+#argC+=1
+#if len(sys.argv) > argC :
+#    offsetStep=int(sys.argv[argC])  
 
 if(not os.path.exists(destination)):
     os.system("mkdir -p "+destination)
+
 destination=os.path.abspath(destination)
 
+
+print(" Executable ",executable)
 print("Source file list ",FileSource)
+print("Run Script Template ",runScriptTemplate)
+print("Configuration ",cfgScriptTemplate )
 print("destination : ",destination)
 print("NJOBS : ",NJOBS)
 print("FILES_PER_JOB : ",FILES_PER_JOB)
@@ -138,6 +178,7 @@ for ii in range(NJOBS):
     tmp=configurationTxt.replace("@@FNAMES",tmp[:-1])
     tmp=tmp.replace("@@TAG",tag)
     tmp=tmp.replace("@@IDX",str(i))
+    tmp=tmp.replace("@@OFFSET_IDX",str(i*offsetStep))
     tmp=tmp.replace("@@MAXEVENTS",str(NEVENTS_PER_JOB))
     cfgFile.write(tmp)
     cfgFile.close()   
